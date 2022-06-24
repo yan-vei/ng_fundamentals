e9a20039b57b67e814e8778d4f5fd492d9df4a17
Yana Veitsman <veytsman.karaganda@gmail.com>
2022-06-24 21:59:18 +0300

    Angular components communication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { EventsListComponent } from './events/events-list.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent
   ],
   bootstrap: [EventsAppComponent]
 })
--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'events-app',
   template: `
-  <h2>Hello World</h2>
-  <img src="assets/images/basic-shield.png"/>
+  <nav-bar></nav-bar>
+  <events-list></events-list>
   `
 })
 export class EventsAppComponent {
