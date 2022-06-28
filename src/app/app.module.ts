ac33da801d81555bb93fc7e5a2d4e4c750e35422
Yana Veitsman <veytsman.karaganda@gmail.com>
2022-06-28 19:38:53 +0300

    Creating Reusable Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { EventService } from './events/shared/event.service';
 import { EventsAppComponent } from './events-app.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { ToastrService } from './common/toastr.service';
 
 @NgModule({
   imports: [
@@ -16,6 +17,7 @@ import { NavBarComponent } from './nav/navbar.component';
     EventThumbnailComponent,
     NavBarComponent
   ],
+  providers: [EventService, ToastrService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
